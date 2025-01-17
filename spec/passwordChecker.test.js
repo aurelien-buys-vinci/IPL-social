1a5734b7433b63f4dc221f22a6e13f7c0ce379d3
buys <aurelien.buys@student.vinci.be>
2025-01-17 17:11:12 +0100

    add: return true when password has 1 specila character

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -13,3 +13,10 @@ describe("list of test for minimum length of passwordChecker", function() {
         expect(checkPassword("123456789")).toBe(true);
     });
 });
+
+describe("list of test for minimum one special character passwordChecker", function() {
+    it("should return true if password has one special character", function() {
+        expect(checkPassword("12345678@")).toBe(true);
+    });
+});
+            
